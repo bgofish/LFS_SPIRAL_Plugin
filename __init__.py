eeda84d7acc179140633026730f2841247e30903
bb6 <73919236+bgofish@users.noreply.github.com>
2026-04-10 21:04:31 +1200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,18 +2,18 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import lichtfeld as lf
-from .transform_panel import TransformPanel
+from .camera_gen_panel import CameraGenPanel
 
-_classes = [TransformPanel]
+_classes = [CameraGenPanel]
 
 
 def on_load():
     for cls in _classes:
         lf.register_class(cls)
-    lf.log.info("EDIT loaded")
+    lf.log.info("camera_json_gen loaded")
 
 
 def on_unload():
     for cls in reversed(_classes):
         lf.unregister_class(cls)
-    lf.log.info("EDIT unloaded")
+    lf.log.info("camera_json_gen unloaded")
